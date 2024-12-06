e21f157ac255fc849bb6b40d786b7beeb0a4b4b3
Lw-wells <wells.lincholmn@students.jkuat.ac.ke>
2024-12-06 19:06:32 +0300

    second commit

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.views.generic import DeleteView
 
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
-
+from django.contrib.auth.views import LogoutView
 urlpatterns=[
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('create-task/', TaskList.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView, LogoutView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
 class CustomLoginView(LoginView):
@@ -19,26 +21,38 @@ class CustomLoginView(LoginView):
 
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
    model = Task
    context_object_name = 'task'
 
-class TaskDetail(DetailView):
+   def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['tasks'] = context['tasks'].filter(user=request.user)
+      context['count'] = context['tasks'].filter(complete=False).count()
+
+      return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
    model = Task
    context_object_name = 'task'
    template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
    model = Task
-   fields = '__all__'
+   fields = ['title', 'description', 'complete']
    success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+   def form_valid(self, form):
+      form.instance.user = self.request.user
+      return super(TaskCreate, self).form_valid(form)
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
    model = Task
-   fields = '__all__'
+   fields = ['title', 'description', 'complete']
    success_url = reverse_lazy('tasks')
 
-class DeleteTask(DeleteView):
+class DeleteTask(LoginRequiredMixin, DeleteView):
    model = Task
    context_object_name = 'task'
    success_url = reverse_lazy('tasks')
